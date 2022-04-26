e007c2e1ed90648ca77a4db9be024719e7a74a6c
Sean <seanlengws@gmail.com>
2022-04-26 23:54:31 +0800

    Added introduction page

--- theme.config.js
+++ theme.config.js
@@ -1,27 +1,37 @@
 // theme.config.js
 export default {
-    projectLink: "https://github.com/helloitsm3/buildspace-faq", // GitHub link in the navbar
+    github: "https://github.com/helloitsm3/buildspace-faq", // GitHub link in the navbar
     docsRepositoryBase: "https://github.com/helloitsm3/buildspace-faq/blob/master", // base URL for the docs repository
-    titleSuffix: " – Nextra",
+    titleSuffix: " – Buildspace FAQ",
     nextLinks: true,
     prevLinks: true,
     search: true,
     customSearch: null, // customizable, you can use algolia for example
     darkMode: true,
     footer: true,
-    footerText: `MIT ${new Date().getFullYear()} © CodePerfect.`,
+    footerText: (
+        <>
+            <span>
+                MIT {new Date().getFullYear()} ©{" "}
+                <a href="https://github.com/helloitsm3" target="_blank" rel="noopener noreferrer">
+                    CodePerfect
+                </a>
+                .
+            </span>
+        </>
+    ),
     footerEditLink: `Edit this page on GitHub`,
     logo: (
         <>
-            <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
-            <span className="text-gray-600 font-normal hidden md:inline">The Next.js Static Site Generator</span>
+            <span className="mr-2 font-extrabold hidden md:inline">Buildspace FAQ</span>
+            <span className="text-gray-600 font-normal hidden md:inline">All your questions answered</span>
         </>
     ),
     head: (
         <>
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <meta name="description" content="Nextra: the next docs builder" />
-            <meta name="og:title" content="Nextra: the next docs builder" />
+            <meta name="description" content="Buildspace FAQ: All your questions answered" />
+            <meta name="og:title" content="Buildspace FAQ: All your questions answered" />
         </>
     ),
     unstable_faviconGlyph: "👋",
