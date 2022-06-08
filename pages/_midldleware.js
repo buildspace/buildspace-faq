bb376e5ec5999732e384d523de9cf911d92401e1
Tair Asim <hi@tairasim.com>
2022-06-08 18:43:08 +0200

    Remove middleware add package-lock

--- pages/_midldleware.js
+++
@@ -1,8 +0,0 @@
-export function middleware() {
-  if (req.nextUrl.href.includes("/help-assets/_next/"))
-    return NextResponse.rewrite(
-      req.nextUrl.href.replace("/help-assets/_next/", "/_next/"),
-    );
-
-  return null;
-}
