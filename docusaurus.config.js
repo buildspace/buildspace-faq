d7a3b343ab2165874fadc8e081de4f61aee589c8
Tair Asim <hi@tairasim.com>
2022-07-13 18:11:07 +0200

    Change baseUrl for the proxy to work

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,12 +9,12 @@ const config = {
   title: 'buildspace localhost',
   tagline: 'Fix your issues and get off localhost!',
   url: 'https://buildspace.so/help',
-  baseUrl: '/',
+  baseUrl: '/help/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon2.png',
   organizationName: 'almostefficient',
-  projectName: 'buildspace-faq', 
+  projectName: 'buildspace-faq',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -33,16 +33,14 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/buildspace/buildspace-faq',
+          editUrl: 'https://github.com/buildspace/buildspace-faq',
           routeBasePath: '/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/buildspace/buildspace-faq',
+          editUrl: 'https://github.com/buildspace/buildspace-faq',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
