050c6ce8a71fe459ee05bc4bffcdb26fab71603b
Sean <seanlengws@gmail.com>
2022-07-22 17:25:01 +0800

    Added algolia search in docusaurus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,9 +13,15 @@ const config = {
     onBrokenLinks: "throw",
     onBrokenMarkdownLinks: "warn",
     favicon: "img/favicon-shipyard.ico",
+
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
     organizationName: "buildspace",
     projectName: "buildspace-faq",
-    themes: ["@docusaurus/theme-classic"],
+
+    // Even if you don't use internalization, you can use this field to set useful
+    // metadata like html lang. For example, if your site is Chinese, you may want
+    // to replace "en" with "zh-Hans".
     i18n: {
         defaultLocale: "en",
         locales: ["en"],
@@ -23,8 +29,9 @@ const config = {
 
     presets: [
         [
-            "@docusaurus/theme-search-algolia",
-            {
+            "classic",
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
                 docs: {
                     sidebarPath: require.resolve("./sidebars.js"),
                     editUrl: "https://github.com/buildspace/buildspace-faq",
@@ -37,94 +44,97 @@ const config = {
                 theme: {
                     customCss: require.resolve("./src/css/custom.css"),
                 },
-            },
+            }),
         ],
     ],
 
-    themeConfig: {
-        algolia: {
-            appId: "UXEM07LQ1Q",
-            apiKey: "cb4e627bf94e42f8699a376d4799e0e6",
-            indexName: "buildspace help",
-            contextualSearch: true,
-            searchPagePath: "search",
-        },
-        navbar: {
-            title: "localhost",
-            logo: {
-                alt: "buildspace localhost Logo",
-                src: "img/logo-light.svg",
-                srcDark: "img/logo-dark.svg",
+    themeConfig:
+        /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ({
+            // algolia: {
+            //     appId: "UXEM07LQ1Q",
+            //     apiKey: "cb4e627bf94e42f8699a376d4799e0e6",
+            //     indexName: "buildspace help",
+            //     contextualSearch: true,
+            //     searchPagePath: "search",
+            // },
+
+            algolia: {
+                appId: "SS3UZTTJJ9",
+                apiKey: "a80d085ec7be3a5367479de9aa724a37",
+                indexName: "dev_buildspacehelp",
+                contextualSearch: true,
+                searchPagePath: "search",
             },
-            items: [
-                // {
-                //   type: 'doc',
-                //   docId: 'intro',
-                //   position: 'left',
-                //   label: 'Tutorial',
-                // },
-                // {to: '/blog', label: 'Blog', position: 'left'},
-                {
-                    href: "https://github.com/buildspace/buildspace-faq",
-                    label: "GitHub",
-                    position: "right",
+            navbar: {
+                title: "localhost",
+                logo: {
+                    alt: "buildspace localhost Logo",
+                    src: "img/logo-light.svg",
+                    srcDark: "img/logo-dark.svg",
                 },
-            ],
-        },
-        footer: {
-            style: "dark",
-            links: [
-                {
-                    title: "Buildspace links",
-                    items: [
-                        {
-                            label: "Buildspace projects",
-                            to: "https://buildspace.so/p?utm_source=raza&utm_medium=footer&utm_campaign=search-buildspace",
-                        },
-                        {
-                            label: "Buildspace jobs",
-                            to: "https://buildspace.so/jobs?utm_source=raza&utm_medium=footer&utm_campaign=search-buildspace",
-                        },
-                        {
-                            label: "Buildspace discover",
-                            to: "https://buildspace.so/discover?utm_source=raza&utm_medium=footer&utm_campaign=search-buildspace",
-                        },
-                    ],
-                },
-                {
-                    title: "Community",
-                    items: [
-                        {
-                            label: "Discord",
-                            href: "https://discord.gg/buildspace",
-                        },
-                        {
-                            label: "Twitter",
-                            href: "https://twitter.com/_buildspace",
-                        },
-                    ],
-                },
-                {
-                    title: "More",
-                    items: [
-                        // {
-                        //   label: 'Blog',
-                        //   to: '/blog',
-                        // },
-                        {
-                            label: "GitHub",
-                            href: "https://github.com/buildspace/buildspace-faq",
-                        },
-                    ],
-                },
-            ],
-            copyright: `Copyright © ${new Date().getFullYear()} buildspace.`,
-        },
-        prism: {
-            theme: lightCodeTheme,
-            darkTheme: darkCodeTheme,
-        },
-    },
+                items: [
+                    {
+                        href: "https://github.com/buildspace/buildspace-faq",
+                        label: "GitHub",
+                        position: "right",
+                    },
+                ],
+            },
+            footer: {
+                style: "dark",
+                links: [
+                    {
+                        title: "Buildspace links",
+                        items: [
+                            {
+                                label: "Buildspace projects",
+                                to: "https://buildspace.so/p?utm_source=raza&utm_medium=footer&utm_campaign=search-buildspace",
+                            },
+                            {
+                                label: "Buildspace jobs",
+                                to: "https://buildspace.so/jobs?utm_source=raza&utm_medium=footer&utm_campaign=search-buildspace",
+                            },
+                            {
+                                label: "Buildspace discover",
+                                to: "https://buildspace.so/discover?utm_source=raza&utm_medium=footer&utm_campaign=search-buildspace",
+                            },
+                        ],
+                    },
+                    {
+                        title: "Community",
+                        items: [
+                            {
+                                label: "Discord",
+                                href: "https://discord.gg/buildspace",
+                            },
+                            {
+                                label: "Twitter",
+                                href: "https://twitter.com/_buildspace",
+                            },
+                        ],
+                    },
+                    {
+                        title: "More",
+                        items: [
+                            // {
+                            //   label: 'Blog',
+                            //   to: '/blog',
+                            // },
+                            {
+                                label: "GitHub",
+                                href: "https://github.com/buildspace/buildspace-faq",
+                            },
+                        ],
+                    },
+                ],
+                copyright: `Copyright © ${new Date().getFullYear()} buildspace.`,
+            },
+            prism: {
+                theme: lightCodeTheme,
+                darkTheme: darkCodeTheme,
+            },
+        }),
 };
 
 module.exports = config;
